81b696be1e84909ab2aca61e4f69e2d6855c5f4f
AnasDORBANI <anasdorbani@gmail.com>
2022-02-27 12:52:57 +0100

    add png icon

--- main/RSA_V4.0.1.py
+++ main/RSA_V4.0.1.py
@@ -4,9 +4,7 @@ from tkinter import filedialog
 from tkinter import messagebox
 from base64 import b64encode, b64decode
 import hashlib
-from Crypto.Cipher import AES
 import os
-from Crypto.Random import get_random_bytes
 import random
 import pathlib
 import sqlite3
